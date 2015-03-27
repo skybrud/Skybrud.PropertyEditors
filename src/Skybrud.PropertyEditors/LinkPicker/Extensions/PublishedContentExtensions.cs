cb42bfedae54654db3355fea9cac9e5bc1d43eb3
Anders Bjerner <abjerner@skybrud.dk>
2015-03-27 12:50:57 +0100

    Initial code commit

---
+++ src/Skybrud.PropertyEditors/LinkPicker/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace Skybrud.PropertyEditors.LinkPicker.Extensions {
+
+    /// <summary>
+    /// Various extension methods for <code>IPublishedContent</code> and the LinkPicker.
+    /// </summary>
+    public static class PublishedContentExtensions {
+
+        /// <summary>
+        /// Gets the first link item from a LinkPicker model from the property with the specified <code>propertyAlias</code>.
+        /// </summary>
+        /// <param name="content">The published content to read the property from.</param>
+        /// <param name="propertyAlias">The alias of the property.</param>
+        public static LinkPickerItem GetLinkPickerItem(this IPublishedContent content, string propertyAlias) {
+            return content == null ? null : GetLinkPickerModel(content, propertyAlias).Items.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets the LinkPicker model from the property with the specified <code>propertyAlias</code>.
+        /// </summary>
+        /// <param name="content">The published content to read the property from.</param>
+        /// <param name="propertyAlias">The alias of the property.</param>
+        public static LinkPickerModel GetLinkPickerModel(this IPublishedContent content, string propertyAlias) {
+            return (content == null ? null : content.GetPropertyValue<LinkPickerModel>(propertyAlias)) ?? new LinkPickerModel {
+                Items = new LinkPickerItem[0]
+            };
+        }
+
+    }
+
+}
